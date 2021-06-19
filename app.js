d594081616043f40820faccab7f1da7f9aedd72d
Shanon Place <shanonplace@gmail.com>
2021-06-18 17:15:41 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/hooks');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+app.use('/hooks', usersRouter);
+
+module.exports = app;

b29a00d695302f5f195135ef4b7094ed9a0fe57d
Shanon Place <shanonplace@gmail.com>
2021-06-19 16:59:48 -0700

    Adding in an example of the config file that isn't part of the .gitignore for reference

---
+++ config.example.js
@@ -0,0 +1,9 @@
+const config = {
+    contentful: {
+      tagName: "article",
+      spaceId: "your-space-id",
+      environment: "master",
+      CMAKey: "your-content-management-api-key"
+    },
+   };
+   module.exports = config;
